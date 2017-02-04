c0ae69d7ee630c470e4a96a2e03434dacd364b80
Seneca Meeks <senecacmeeks@gmail.com>
2017-02-04 15:03:31 -0500

    Adding a database to our GymBuddy app

---
+++ DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  GymBuddy1.0
+//
+//  Created by Seneca Meeks on 2/4/17.
+//  Copyright © 2017 Hidden Figures. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+-(instancetype)GymInfo:(NSString *)GymInfo;
+@end
---
+++ DBManager.m
@@ -0,0 +1,19 @@
+//
+//  DBManager.m
+//  GymBuddy1.0
+//
+//  Created by Seneca Meeks on 2/4/17.
+//  Copyright © 2017 Hidden Figures. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+-(instancetype)GymInfo:(NSString *)GymInfo{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
